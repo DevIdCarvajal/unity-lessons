994f75d0707d3683378dbe8119a40f579c82a27c
David Carvajal Garrido <davidcarvajalg@gmail.com>
2022-05-19 22:01:44 +0200

    New examples & commented code

--- examples/02-virtual-darts/src/Assets/Scripts/Dartboard.cs
+++ examples/02-virtual-darts/src/Assets/Scripts/Dartboard.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Dartboard : MonoBehaviour
 {
+    // ESTADOS
     public int points = 0;
     public int turn = 1;
     public bool endTurn = false;
-    public GameObject dartPrefab;
 
+    // DATOS DE INSTANCIACIÓN DE NUEVO DARDO
+    public GameObject dartPrefab;
     Vector3 dartPosition = new Vector3(0, 1.8f, -8.75f);
     Quaternion dartRotation = Quaternion.Euler(0, 90, 0);
 
@@ -19,19 +21,25 @@ public class Dartboard : MonoBehaviour
 
     void Update()
     {
+        // Si se acabó el turno
         if( endTurn == true )
         {
+            // Si se pulsa la tecla espacio
             if( Input.GetKeyDown( KeyCode.Space ) )
             {
+                // Instanciar objeto
                 Instantiate(dartPrefab, dartPosition, dartRotation);
 
+                // Acabar el turno
                 endTurn = false;
             }
         }
     }
 
+    // Al colisionar
     void OnCollisionEnter(Collision collision)
     {
+        // Aumentar puntos, aumentar el turno y acabar el turno
         points++;
         turn++;
         endTurn = true;
--- examples/02-virtual-darts/src/Assets/Scripts/StartButton.cs
+++ examples/02-virtual-darts/src/Assets/Scripts/StartButton.cs
@@ -15,6 +15,7 @@ public class StartButton : MonoBehaviour
         
     }
 
+    // OBLIGATORIO que sea public
     public void LoadMainGame()
     {
         SceneManager.LoadScene("MainGame");
