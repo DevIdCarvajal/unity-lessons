94f1f0f74642db48f4e0e2f974d5d07b6e2dd8ee
David Carvajal Garrido <davidcarvajalg@gmail.com>
2022-06-15 17:14:51 +0200

    Raycasting & more

--- examples/00-sandbox/Assets/Scripts/Movement/CharacterLookAt.cs
+++ examples/00-sandbox/Assets/Scripts/Movement/CharacterLookAt.cs
@@ -1,34 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class CharacterLookAt : MonoBehaviour
 {
     [SerializeField]
     bool isLooking = false;
 
-    /* ------------ Example 1: BAD ------------ */
-
-    // float horizontalSpeed = 2.0f;
-    // float verticalSpeed = 2.0f;
-
-    /* ------------ Example 2: GOOD ------------ */
+    [SerializeField]
+    Transform character;
+    
+    [SerializeField]
+    TMP_Text debugPanelText;
 
     float mouseX = 0;
     float mouseY = 0;
     float sensitivity = 2;
 
-    [SerializeField]
-    Transform character;
+    Ray ray;
 
     void Start()
     {
-        
+        // Creates a Ray from the character to forward
+        //ray = new Ray(transform.position, transform.forward);
     }
 
     void Update()
     {
-        // Si el jugador hace click izquierdo con el ratón
+        // Toggle between first/third person cameras
         if (Input.GetButton("Fire1"))
         {
             isLooking = !isLooking;
@@ -36,22 +35,62 @@ public class CharacterLookAt : MonoBehaviour
         
         if (isLooking)
         {
-            /* ------------ Example 1: BAD ------------ */
-            
-            // float h = horizontalSpeed * Input.GetAxis("Mouse X");
-            // float v = verticalSpeed * Input.GetAxis("Mouse Y");
+            // Horizontal look
+            if(Input.mousePosition.x < 10) // Left limit
+            {
+                mouseX -= sensitivity;
+            }
+            else
+            if(Input.mousePosition.x > Screen.width-10) // Right limit
+            {
+                mouseX += sensitivity;
+            }
+            else // Between limits
+            {
+                mouseX += Input.GetAxis("Mouse X") * sensitivity;
+            }
 
-            // transform.Rotate(v, h, 0);
-
-            /* ------------ Example 2: GOOD ------------ */
-
-            mouseX += Input.GetAxis("Mouse X") * sensitivity;
+            // Vertical look
             mouseY -= Input.GetAxis("Mouse Y") * sensitivity;
-
             //mouseY = Mathf.Clamp(mouseY, -30f, 30f);
 
+            // Update camera
             character.localEulerAngles = new Vector3(0, mouseX, 0);
             transform.localEulerAngles = new Vector3(mouseY, 0, 0);
+
+            // ------------------- Debugging -------------------
+            // Debug.Log(Screen.width);
+
+            debugPanelText.text =
+                "X: " + Input.mousePosition.x +
+                " - " +
+                "Y: " + Input.mousePosition.y;
+        }
+        
+        // Raycasting
+        if(Input.GetKeyDown(KeyCode.R)) {
+            // ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            ray = new Ray(transform.position, transform.forward);
+
+            // RaycastHit hitData;
+
+            // if (Physics.Raycast(ray, out hitData))
+            // {
+            //     // The Ray hit something!
+            //     print("Shoot!");
+
+            //     // Vector3 hitPosition  = hitData.point;
+            //     // float hitDistance    = hitData.distance;
+            //     // string targetTag     = hitData.collider.tag;
+            //     // GameObject hitObject = hitData.transform.gameObject;
+
+            //     // Debug.Log(hitPosition);
+            //     // Debug.Log(hitDistance);
+            //     // Debug.Log(targetTag);
+            //     // Destroy(hitObject);
+            // }
+
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
         }
     }
 }
