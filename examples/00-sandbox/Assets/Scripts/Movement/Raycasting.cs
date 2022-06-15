9690c124d557c931dbeb2439d68b6d9ffa1e1d0e
David Carvajal Garrido <davidcarvajalg@gmail.com>
2022-06-15 21:38:05 +0200

    Refactoring & TODOing (...more)

--- examples/00-sandbox/Assets/Scripts/Movement/Raycasting.cs
+++ examples/00-sandbox/Assets/Scripts/Movement/Raycasting.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Raycasting : MonoBehaviour
 {
+    Ray ray;
+
     void Start()
     {
-        
+        // Creates a Ray from the character to forward
+        // ray = new Ray(transform.position, transform.forward);
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)) {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //ray = new Ray(transform.position, transform.forward);
+
+            RaycastHit hitData;
+
+            // If the Ray hit something...
+            if (Physics.Raycast(ray, out hitData))
+            {
+                // Debug.Log(hitPosition);
+                // Debug.Log(hitDistance);
+
+                if (hitData.collider.tag == "Destructible")
+                {                    
+                    Destroy(hitData.transform.gameObject);
+                }
+            }
+            
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
+        }
     }
 }
